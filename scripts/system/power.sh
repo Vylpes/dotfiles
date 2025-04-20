db0ad5baf31b802c1921a5da00a414537ecc6e89
Ethan Lane <ethan@vylpes.com>
2025-04-20 13:05:13 +0100

    Update lock command to use i3lock over i3lock-fancy

--- scripts/system/power.sh
+++ scripts/system/power.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-option=$(printf "sleep\nlogout\nshutdown\nrestart" | rofi -dmenu -theme Arc-Dark -font 'hack 18')
+option=$(printf "lock\nsleep\nlogout\nshutdown\nrestart" | rofi -dmenu -theme Arc-Dark -font 'hack 18')
 
-if [ "$option" = "sleep" ];
+if [ "$option" = "lock" ];
 then
-    i3lock-fancy -g -t "System Locked" -f Hack-Regular
+    i3lock --color 000000
+elif [ "$option" = "sleep" ];
+then
+    i3lock --color 000000
     systemctl suspend
 elif [ "$option" = "logout" ];
 then
